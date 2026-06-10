882cc20ad0ca85e33758382f06e11f31624e6f38
Md Nouman <noumantannu06@gmail.com>
2026-06-10 21:14:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,6 +7,15 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
+// team stores a reference to players
+const team = players;
+
+// team1 is a copy of players (shallow copy)
+const team1 = [...players];
+
+// cap1 is a copy of the person object (shallow copy)
+const cap1 = { ...person };
+
 window.players = players;
 window.person = person;
 window.team = team;
